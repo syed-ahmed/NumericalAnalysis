73449bd03db2638fc9c05a18988631e026b9a1ab
kab2788 <kab2788@rit.edu>
2016-09-04 15:09:04 -0400

    Break Math

---
+++ BreakMath.cpp
@@ -0,0 +1,20 @@
+#include "math.h"
+//#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// No need to pass in variables
+int main() {
+
+int num1 = 5;
+int num2 = 2;
+int result;
+
+result = 5/2;
+
+cout<<"result/n"<<result;
+
+
+}

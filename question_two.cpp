9c4f4d512239050eb4cff838e56ce50b008df9a8
Syed Tousif Ahmed <luna@laluna.local>
2016-09-10 20:27:59 -0400

    Refactoring code (it was simpler than i thought after confirming with professor.)

--- question_two.cpp
+++ question_two.cpp
@@ -3,49 +3,15 @@
 
 using namespace std;
 
-int CalculateFactorial(int input_number)
-{
-    int result;
-    if (input_number == 0 || input_number == 1)
-    {
-        return 1;
-    }
-    result = CalculateFactorial(input_number - 1) * input_number;
-    return result;
-}
-
-int CalculateCombination(int n, int r)
-{
-    int result;
-    if (n >= 0 && r >= 0)
-    {
-        result = CalculateFactorial(n) / (CalculateFactorial(r) * CalculateFactorial(n - r));
-    }
-    else
-    {
-        result = 0;
-    }
-    return result;
-}
-
-float CalculatePi(int num_iterations)
-{
-    float pi_value = 0;
-    for (int i = 0; i < num_iterations; i++)
-    {
-        pi_value += (3 * CalculateCombination(2 * i, i)) / (pow(16, i) * (2 * i + 1));
-    }
-    return pi_value;
-}
-
 int main(int argc, char *argv[])
 {
     cout << "Hello World!" << endl;
     int i;
-    cout << "Please enter the number of iterations you want to use to approximate pi: ";
+    cout << "Please enter an integer number: ";
     cin >> i;
     cout << "The value you entered is " << i << endl;
     cout << "Now we are going to approximate the number pi using inverse trignometry" << endl;
-    printf("PI = %f\n", CalculatePi(i));
+    cout << "Printing at 9 significant digits..."  << endl;
+    printf("PI = %.8f\n", 6.0*asin(0.5));
     return 0;
 }
